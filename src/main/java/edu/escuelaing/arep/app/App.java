0796e7b3e494c3b7799cb239d8b9003db3f789d6
Nikolai9906 <nicohhhh1@gmail.com>
2021-01-30 23:01:37 -0500

    SparWeb implemented

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,13 +1,18 @@
 package edu.escuelaing.arep.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import static spark.Spark.*;
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+        public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
     }
+
+        static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
